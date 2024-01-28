4beda8a50030ea79fbf157e9419e4f043f713e3d
Andrei Kharitonov <pharret31@gmail.com>
2024-01-28 22:38:19 +0200

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana, Mulish } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin'] })
+export const lusitana = Lusitana({ weight: ['400', '700'], subsets: ['latin'] })
+export const mulish = Mulish({ subsets: ['latin'] })
